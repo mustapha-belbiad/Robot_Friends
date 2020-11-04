531bad99d88909cc88aede87f438507de59aafac
mustapha-belbiad <mustapha.belbiad10@gmail.com>
2020-11-04 14:04:22 +0100

    add files

---
+++ src/components/Searchbox.js
@@ -0,0 +1,15 @@
+import React from 'react' ;
+ 
+const Searchbox = ({searhfield, searchchange}) => {
+	return (
+		<div className='pa2'>
+		 <input
+		 className='pa3 ba b--green bg-lightest-blue'  
+		 type ='search' 
+		 placeholder ='Robots friends'
+		 onChange={searchchange}
+		 />
+		</div>
+	 )
+};
+export default Searchbox;
